93e36333943d6a8f794015807fe810dd9c5fc052
Fabian Fuenmayor <fabianfuen19@icloud.com>
2025-03-10 09:33:48 +0000

    book_appointment changes

--- src/front/js/component/book_appointment.jsx
+++ src/front/js/component/book_appointment.jsx
@@ -20,7 +20,7 @@ export const BookAppointment = () => {
 
                 if (store.user?.perfil_especialista?.id) {
                     console.log("Cargando disponibilidad del médico con ID:", store.user.perfil_especialista.id);
-                    actions.fetchAvailability(store.user.perfil_especialista.id); 
+                    actions.fetchAvailability(store.user.perfil_especialista.id);
                 } else {
                     console.error("Error: El usuario no tiene un perfil de especialista registrado.");
                 }
